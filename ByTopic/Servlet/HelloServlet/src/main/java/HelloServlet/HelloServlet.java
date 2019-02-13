9ed1cf6de902ae2814fe95446de400a2f36b4131
wangqian <wangqian2@guosen.com.cn>
2019-02-13 15:12:17 +0800

    add samples

---
+++ ByTopic/Servlet/HelloServlet/src/main/java/HelloServlet/HelloServlet.java
@@ -0,0 +1,15 @@
+package HelloServlet;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        resp.setContentType("text/text");
+        PrintWriter writer = resp.getWriter();
+        writer.println("ok");
+    }
+}
