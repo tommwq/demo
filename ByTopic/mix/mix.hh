4b98589b61dc4f64020fd89bcc8ce3986234e4de
wangqian <wangqian2@guosen.com.cn>
2019-05-21 09:44:07 +0800

    add

---
+++ ByTopic/mix/mix.hh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "type.hh"
+
+class Mix {
+private:
+    Word rA;
+    Word rJ;
+    Word rI[6];
+    Word rJ;
+    Toogle overflow;
+    Indicator compare;
+    Word memory[4000];
+};
---
+++ ByTopic/mix/type.cc
@@ -0,0 +1,6 @@
+#include "type.hh"
+
+const unsigned int Byte::Max = 63;
+const unsigned int Byte::Max2 = 4095;
+const unsigned int Byte::Max3 = 16777215;
+const unsigned int Byte::Max4 = 1073741823;
