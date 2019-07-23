a360d0e5ce382af16da622a567fabae8a0326656
tommwq <tommwq@126.com>
2019-07-24 07:23:35 +0800

    add

--- ByTopic/Samples/MakingOSIn30Days/2/b.asm
+++ ByTopic/Samples/MakingOSIn30Days/2/b.asm
@@ -1,40 +1,17 @@
     org 0x8000
 
-    jmp entry
-    db 0x90
-
-entry:
+    ;; setup stack
     mov ax,0x00
     mov ss,ax
     mov sp,0x8000
     mov ds,ax
     mov es,ax
 
-    ;; 显示Hello, world!
-    mov al, 0
-    mov si, msg
-
-putloop:
-    mov al, [si]
-    add si, 1
-    cmp al, 0
-
-    je fin
-    mov ah, 0x0e
-    mov bx, 15
-    int 0x10
-    jmp putloop
-
+    ;; set display mode
     mov ah, 0x00
     mov al, 0x13
     int 0x10
-    
-fin:
-    hlt
-    jmp fin
-msg:
-    db 0x0a, 0x0a
-    db "ok"
-    db 0x0a
-    db 0
 
+over:   
+    hlt
+    jmp over
