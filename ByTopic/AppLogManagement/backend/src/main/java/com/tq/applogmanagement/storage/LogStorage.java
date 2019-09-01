5392b28d1e09d54cfdafac98369dbfa1479ab7da
tommwq <tommwq@126.com>
2019-09-02 01:26:32 +0800

    split project

--- ByTopic/AppLogManagement/backend/src/main/java/com/tq/applogmanagement/storage/LogStorage.java
+++
@@ -1,9 +0,0 @@
-package com.tq.applogmanagement.storage;
-
-import com.tq.applogmanagement.AppLogManagementProto.Log;
-import java.util.List;
-
-public interface LogStorage {
-  void save(Log log);
-  List<Log> load(String deviceId, long sequence, int count);
-}
--- ByTopic/AppLogManagement/backend/src/main/resources/template/view/log.ftl
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <body>
-    日志 <hr/>
-    <#list logs as log>
-      ${log} <br />
-    </#list>
-  </body>
-</html>
